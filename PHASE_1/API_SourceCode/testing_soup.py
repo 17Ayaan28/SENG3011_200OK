edc8923443e94356e14ea517662a8f1048671b83
Ayaan Adil <ayaanadil@Ayaans-MacBook-Air.local>
2021-03-10 17:33:55 +0530

    final

--- PHASE_1/API_SourceCode/testing_soup.py
+++ PHASE_1/API_SourceCode/testing_soup.py
@@ -1,6 +1,11 @@
-from bs4 import BeautifulSoup
+## Libraries required for web scraping
+from bs4 import BeautifulSoup 
 import requests
 
+## Used to convert python dicts to json
+import json
+
+## Used to connect to python database
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -11,23 +16,26 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://quick-9e9b1-default-rtdb.firebaseio.com/'
 })
 
-ref = db.reference("/")
-ref.set({
-	"test":
-	{
-		"test": 1
-	}
-})
 
-url = "https://www.cdc.gov/outbreaks/"
 
-cdc_r = requests.get(url)
+base_url = "https://www.cdc.gov/outbreaks/"
+
+cdc_r = requests.get(base_url)
 
 
 
-cdc_soup = BeautifulSoup(cdc_r.text, 'html.parser')
+soup = BeautifulSoup(cdc_r.text, 'html.parser')
 
 
-for link in cdc_soup.find_all('a', attrs={'class':'feed-item-title'}):
-	title = link.getText()
-	print(title)
+#print(cdc_soup.prettify())
+outbreak_links_list = soup.find('ul',class_="list-bullet feed-item-list")  ## gets the ul that contains the list of outbreaks along with html
+
+
+for link in outbreak_links_list.find_all('a'):
+
+	print(link.getText()) ## gets title of text
+	print(link.get('href'))
+	## also extract date
+
+	link_to_next_page = requests.get(base_url + link.get('href'))
+	soup_2 = BeautifulSoup(link_to_next_page, 'html.parser')
